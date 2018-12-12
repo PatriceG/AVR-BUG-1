10b05232b017551ae30ec4a6e680f8be8f84dc41
Patrice GODARD <patrice.godard@orange.com>
2018-12-12 15:27:14 +0100

    Ajoutez des fichiers projet.

---
+++ AVR_Bug_1/AVR_Bug_1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		AVR_Bug_1.ino
+ Created:	12/12/2018 15:27:01
+ Author:	XNRQ8224
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
